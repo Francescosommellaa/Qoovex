9aa1cd383095a3cc461603e94495b2e8898ee0b4
Francesco Sommella <francescosommellaa@gmail.com>
2026-04-11 20:08:42 +0200

    add: Analytics from vercel for sirio

--- apps/sirio/src/app/layout.tsx
+++ apps/sirio/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -19,13 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="it" className="dark">
-      <head>
-        <link
-          href="https://api.fontshare.com/v2/css?f[]=satoshi@400,500,700&f[]=chillax@400,500,600,700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
